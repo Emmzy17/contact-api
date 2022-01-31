45b9a66104b4b58281625611403885dcc7324c97
Emmzy17 <emmanueladjerry@gmail.com>
2022-01-30 23:19:07 -0800

    contact api

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from email import message
 from flask import Flask
 from flask_restful import Api, Resource, abort, reqparse, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
@@ -22,6 +23,12 @@ contact_args.add_argument('name', type =str, help = 'Input right name of contact
 contact_args.add_argument('phone_no', type =int, help = 'Input phone No.', required = True)
 contact_args.add_argument('email', type =str, help = 'Input the email')
 
+contact_update = reqparse.RequestParser()
+contact_update.add_argument('name', type = str)
+contact_update.add_argument('phone_no', type = int)
+contact_update.add_argument('email', type = str)
+
+
 contact_field = {
     'id':fields.Integer,
     'name' : fields.String,
@@ -32,40 +39,48 @@ contact_field = {
 
 class contact(Resource):
     @marshal_with(contact_field)
-    def post(self):
-        #if name:
-        #    return('Name already taken')
+    def get(self, name):
+        result = ContactDb.query.filter_by(name = name).first()
+        if not result:
+            abort(404, message = 'contact not found')
+        return result
+    @marshal_with(contact_field)
+    def put(self, name):
+        
         args = contact_args.parse_args()
+        name = args['name']
+        result = ContactDb.query.filter_by(name = args['name']).first()
+        if result:
+            abort(409, message = 'contact exist')
         contact = ContactDb(name = args['name'], phone_no = args['phone_no'], email = args['email'])
         db.session.add(contact)
         db.session.commit()
         return contact, 201
-    @marshal_with(contact_field)
-    def get(self, name):
+    @marshal_with(contact_field)     
+    def patch(self, name):
+        args = contact_update.parse_args()
         result = ContactDb.query.filter_by(name = name)
         if not result:
-            abort(404, 'contact not found')
-        return result
-    @marshal_with(contact_field)   
-    def patch(self):
-        args = contact_args.parse_args()
-        result = ContactDb.query.filter_by(name = name)
+            abort(404, 'contact does not exist')
         if args['name']:
             result.name = args['name']
         if args['phone_no']:
             result.phone_no = args['phone_no']
         if args['email']:
             result.email = args['email']
+
+        db.session.commit()
         return result
         
         
     def delete(self, name):
-        result = ContactDb.query.filter_by(name = name)
+        result = ContactDb.query.filter_by(name = name).first()
         if not result:
-            abort(404, 'Contact does not exist')
+            abort(404, message ='Contact does not exist')
         del result
-        return '', 201
-api.add_resource(contact,'/')
+        db.session.commit()
+        return {}, 201
+api.add_resource(contact,'/contact', '/name/<string:name>')
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,12 +1,9 @@
-
-from unicodedata import name
 import requests
 
 BASE = 'http://127.0.0.1:5000/'
-response = requests.post(BASE , {'name': 'Emjay', 'phone_no':8164828761, 'email':'emanueladjerry@gmail.com'})
-print(response)
-#response = requests.delete(BASE + 'video/0')
-#print(response)
-#input()
-#response = requests.patch(BASE + 'contact/emmy', {'name': name, 'phone_no': 8164828761})
+#response = requests.patch(BASE + 'name/Emj', {'name' : 'lizza', 'phone_no':90568829, 'email':'bliz23@yahoo.com'})
 #print(response.json())
+#response = requests.get(BASE + 'name/Emj')
+#print(response.json())
+response = requests.delete(BASE + 'name/Emj')
+print(response)
